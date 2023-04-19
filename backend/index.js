c59a0d6e4efdd383c9f3837a9cf2196ad9f991c0
Josemon Baby <babyjosemon77@gmail.com>
2023-04-19 22:12:51 +0100

    update index and package join

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.get("/api", (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../client/build', 'index.js'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(PORT, () => {
